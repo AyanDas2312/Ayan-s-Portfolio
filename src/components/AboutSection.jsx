ba09370e8f0da70e5126f9838211ba44bee0df98
Ayan Das <ayandas6790@gmail.com>
2025-10-10 11:37:40 +0530

    download-cv-enable

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -37,8 +37,8 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href=""
-                className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
+                href="Projects\Resume_AyanDas.pdf"
+                className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300 download"
               >
                 Download CV
               </a>
